32f113ac1ec5b7fc82a119a0343fd19d26cb7c39
janreggie <jan.delacruz@obf.ateneo.edu>
2018-03-15 15:03:00 +0800

    Added `main.cpp`, `polyroot.h`
    sayori==waifu

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "polyroot.h"
+
+int main()
+{
+  std::cout << "Fuck me Vater." << std::endl;
+  return 0;
+}
---
+++ polyroot.h
@@ -0,0 +1,8 @@
+#include <sstream>
+#include <vector>
+#include <complex>
+#include <cmath>
+
+void polyroot (std::string fileName);
+
+std::vector<std::complex> helper_algo (std::vector <double>);
