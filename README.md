b59f8d392d1f34e5a35f5521b86a7ba2b25c4ee8
janreggie <jan.delacruz@obf.ateneo.edu>
2018-03-18 15:32:35 +0800

    Added helper_algo for Durand-Kerner algorithm
    Also added a plea for St. Isidore in the README
    To-do: Implement Durand-Kerner

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # polyroot
+
 Implementation of `polyroot` by Dela Cruz, Park, and Vallente
+
+We bless our patron Saint, St. Isidore of Seville.
--- polyroot.h
+++ polyroot.h
@@ -3,10 +3,38 @@
 #include <complex>
 #include <cmath>
 #include <string>
+#include <functional>
 
-std::vector<std::complex <double> > helper_algo (std::vector <double>)
+std::vector< std::complex <double> > helper_DurandKerner
+(std::vector < std::complex <double> > & rootList,
+ std::function < std::complex<double> (std::complex<double>) > & f)
 {
-  return {};
+  // Thank you St. Isidore.
+}
+
+std::vector<std::complex <double> > helper_algo
+(std::vector <double> coefs)
+{
+  // Create a reduced function i.e. whose leading coefficient is one.
+  // Note that the function takes in complex and returns complex!
+  std::function <std::complex<double> (std::complex<double>) > fxnReduced
+  = [coefs] (std::complex<double> x) -> std::complex<double>
+  {
+    std::complex <double> ret (0,0);
+    for (int i = 0; i < coefs.size(); ++i)
+    {
+      ret += (coefs.at(i) * std::pow(x, i));
+    }
+    ret /= *(coefs.rbegin());
+    return ret;
+  };
+
+  // Our initial roots (as prescribed by Wikipe-tan's article)
+  std::vector< std::complex<double> > rootList(coefs.size()-1);
+  for (int i = 0; i < rootList.size(); ++i)
+    rootList.at(i) = std::pow(std::complex<double>(0.4, 0.9), i+1);
+
+  // Now do some fixed point iteration
 };
 
 void polyroot (std::string fileName)
