70b72dea1a781aa74e205499dcd2b6116e140f40
janreggie <jan.delacruz@obf.ateneo.edu>
2018-03-25 17:38:45 +0800

    Changed README; added Durand-Kerner implementation
    Changed README to display the implementation of the root-finding algorithm
    used in the code.
    
    Added the Durand-Kerner implementation described by Guggenheimer in function
    helper_algo() in polyroot.h. This took way too fucking long to make.

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Implementation of `polyroot` by Dela Cruz, Park, and Vallente
 
-We bless our patron Saint, St. Isidore of Seville.
+## Root-finding algorithm
+
+The root-finding algorithm shall be the Durand-Kerner algorithm, whose
+initial roots are determined using (Guggenheimer's "Initial Approximations
+in Durand-Kerner's Root-Finding Method")[https://doi.org/10.1007/BF01935059].
--- polyroot.h
+++ polyroot.h
@@ -5,34 +5,89 @@
 #include <string>
 #include <functional>
 #include <cassert>
+#include <algorithm>
+
+#define PI
 
 std::vector<std::complex <double> > helper_algo
-(std::vector <double> coefs)
+(const std::vector <double> & coefs)
 {
-  // first, assert whether the function is "vaild"
-  // i.e. the n'th coefficient is not zero
-  assert(*coefs.rbegin() != 0);
-  // Create a reduced function i.e. whose leading coefficient is one.
-  // Note that the function takes in complex and returns complex!
-  std::function <std::complex<double> (std::complex<double>) > fxnReduced
-  = [coefs] (std::complex<double> x) -> std::complex<double>
+  unsigned int n = coefs.size() - 1;  // degree
+  double precision = 1e-6;  // the algo's precision
+  /*
+  This root-finding method is described by Guggenheimer's
+  "Initial Approximations in Durand-Kerner's Root-Finding Method".
+  https://doi.org/10.1007/BF01935059
+  Certain semantics may be off here.
+  */
+
+  // Assert that neither a0 nor an is zero
+  assert(coefs[0] * coefs[n] != 0);
+
+  // Copy coefs, but reverse it.
+  std::vector<double> rcoefs (coefs);
+  std::reverse(std::begin(rcoefs), std::end(rcoefs));
+
+  // Now create a lambda expression for the function
+  std::function< std::complex<double>(std::complex<double>) > p
+  = [rcoefs](std::complex<double> z)
   {
-    std::complex <double> ret (0,0);
-    for (int i = 0; i < coefs.size(); ++i)
-    {
-      ret += (coefs.at(i) * std::pow(x, i));
-    }
-    ret /= *(coefs.rbegin());
-    return ret;
+    std::complex<double> ans (0,0);
+    for (unsigned int i = 0; i < rcoefs.size(); ++i)
+      ans += rcoefs.at(i) * std::pow(z, (rcoefs.size() - 1 - i));
+    return ans;
   };
 
-  // Our initial roots (as prescribed by Wikipe-tan's article)
-  std::vector< std::complex<double> > rootList(coefs.size()-1);
-  for (int i = 0; i < rootList.size(); ++i)
-    rootList.at(i) = std::pow(std::complex<double>(0.4, 0.9), i+1);
+  // Now determine our r0
+  std::vector<double> u;  // step 1a
+  for (unsigned int i = 1; i <= n; ++i)
+    if (rcoefs[i] != 0)
+      u.push_back(1.0 / (2 * std::pow(std::abs(rcoefs[i]/rcoefs[0]) , 1/i)));
+  std::vector<double> v;  // step 1b
+  for (unsigned int j = 0; j < n; ++j)
+    if (rcoefs[j] != 0)
+      v.push_back(1.0 / (2 * std::pow(std::abs(rcoefs[j]/rcoefs[n]), 1/(n-j))));
+  double uAve = std::accumulate(u.begin(), u.end(), 0) / u.size();  // step 2
+  double vAve = std::accumulate(v.begin(), v.end(), 0) / v.size();
+  auto it = u.begin();  // removal of the "offshoot" u_i and v_i
+  for (auto itTmp = u.begin(); itTmp != u.end(); ++itTmp)
+    if (std::abs(*it - uAve) < std::abs(*itTmp - uAve))
+      it = itTmp;
+  u.erase(it);
+  it = v.begin();
+  for (auto itTmp = v.begin(); itTmp != v.end(); ++itTmp)
+    if (std::abs(*it - vAve) < std::abs(*itTmp - vAve))
+      it = itTmp;
+  v.erase(it);
+  uAve = std::accumulate(u.begin(), u.end(), 0) / u.size();
+  vAve = std::accumulate(v.begin(), v.end(), 0) / v.size();
+  double r0 = (uAve + vAve) / 2.0;
 
-  // Now do some fixed point iteration
-  // do_stuff.png
+  // Now create a vector for our roots
+  std::vector< std::complex<double> > roots(n+1);
+  for (unsigned int k = 0; k <= n; ++k)
+    roots[k] = r0 * std::exp(2*M_PI*k*std::complex<double>(0,1)/double(n));
+
+  // Now, time for some fixed-point fucking iteration.
+  while (true)  // >inb4 infinite loop
+  {
+    double lol = 0;  // our maximum difference; ran out of identifiers lol
+    for (unsigned int k = 0; k <= n; ++k)  // implement the fixed point iteration
+    {
+      // find the denominator (long-ass expression in the paper)
+      std::complex<double> denom = rcoefs[0];
+      for (unsigned int l = 0; l <= n; ++l)
+        if (l != k)
+          denom *= (roots[k] - roots[l]);
+      roots[k] = roots[k] - p(roots[k]) / denom;
+      // and lol's one-liner if statement
+      lol = ((std::abs(p(roots[k])/denom) > lol) ? std::abs(p(roots[k])/denom) : lol);
+    }
+    // and break if lol is less than our precision
+    if (lol < precision)
+      break;
+  }
+  return roots;
 };
 
 void polyroot (std::string fileName)
